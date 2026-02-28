13a66d7b4ae77a885c84e07190a318ba8a61a96f
Aman <amanraghav682@gmail.com>
2026-02-28 18:38:38 +0530

    Updated project structure

--- ticket-booking-system/src/main/java/com/ticketbooking/system/config/JwtAuthenticationFilter.java
+++ ticket-booking-system/src/main/java/com/ticketbooking/system/config/JwtAuthenticationFilter.java
@@ -1,39 +1,36 @@
 package com.ticketbooking.system.config;
 
+import com.ticketbooking.system.service.JwtService;
+import com.ticketbooking.system.token.TokenRepository;
 import com.ticketbooking.system.util.ResponseHandler;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.security.SignatureException;
 
 @Component
 @RequiredArgsConstructor
-@Service
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    @Autowired
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
     private final JwtService jwtService;
-
-    @Autowired
     private final UserDetailsService userDetailsService;
-
-    @Autowired
     private final TokenRepository tokenRepository;
 
     @Override
@@ -41,52 +38,43 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain)
             throws ServletException, IOException {
-    try {
-        if(request.getServletPath().contains("/api/v1/auth")){
-            filterChain.doFilter(request, response);
-            return;
-        }
-        final String authHeader = request.getHeader("Authorization");
-        final String jwt;
-        final String username;
-        if(authHeader == null || !authHeader.startsWith("Bearer ")){
-            filterChain.doFilter(request, response);
-            return;
-        }
-        jwt = authHeader.substring(7);
-        username = jwtService.extractUsername(jwt);
-        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-            var isTokenValid = tokenRepository.findByToken(jwt)
-                    .map(t -> !t.isExpired() && !t.isRevoked())
-                    .orElse(false);
-            if(!isTokenValid){
-                logger.error("Session is expired, Please login again");
-                throw new BadCredentialsException("Session is expired, please login again");
+        try {
+            if (request.getServletPath().contains("/api/v1/auth")) {
+                filterChain.doFilter(request, response);
+                return;
             }
-            if(jwtService.isTokenValid(jwt, userDetails)){
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails,
-                        null,
-                        userDetails.getAuthorities()
-                );
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
-                SecurityContextHolder.getContext().setAuthentication(authToken);
-            } else {
-                logger.error("Token is invalid, Please login again");
-                throw new BadCredentialsException("Token is invalid, Please login again");
+            final String authHeader = request.getHeader("Authorization");
+            final String jwt;
+            final String username;
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                filterChain.doFilter(request, response);
+                return;
             }
+            jwt = authHeader.substring(7);
+            username = jwtService.extractUsername(jwt);
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+                var isTokenValid = tokenRepository.findByToken(jwt)
+                        .map(t -> !t.isExpired() && !t.isRevoked())
+                        .orElse(false);
+                if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities()
+                    );
+                    authToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request)
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+            filterChain.doFilter(request, response);
+        } catch (JwtException e) {
+            logger.error("Error processing JWT token: {}", e.getMessage());
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.addHeader("Content-type", "application/json");
+            ResponseHandler.write(response.getOutputStream(), ResponseHandler.generateResponse(e.getMessage(), HttpStatus.UNAUTHORIZED, false, ""));
         }
-        filterChain.doFilter(request, response);
-        } catch (BadCredentialsException | SignatureException be){
-        logger.error("There is token related error, {}", be);
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.addHeader("Content-type", "application/json");
-        ResponseHandler.write(response.getOutputStream(), ResponseHandler.generateResponse(be.getMessage(), HttpStatus.UNAUTHORIZED, false, ""));
-        } finally {
-        SecurityContextHolder.createEmptyContext();
-    }
     }
 }
